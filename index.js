df25408e00a07c37e733bf9decd8282561e13125
Prince72004 <kaushalprince74@gmail.com>
2026-06-23 16:39:28 +0530

    Fix render deployment

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const { dbconnection } = require("./connection/dbconnection");
 const urlroutes = require("./routes/urlroutes");
 const app = express();
-dbconnection("mongodb://127.0.0.1:27017/urlshortner");
+// dbconnection("mongodb://127.0.0.1:27017/urlshortner");
+const PORT = process.env.PORT || 3030;
+dbconnection(process.env.MONGO_URI);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/url", urlroutes);
+app.get("/", (req, res) => {
+    res.send("URL Shortener API Running");
+});
 
 
-app.listen(3030, () => {
-    console.log("server started at 3030 port ");
-})
+
+app.listen(PORT, () => {
+    console.log(`server started at ${PORT}`);
+});
